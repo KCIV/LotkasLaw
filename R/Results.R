537e20ccd3ad7f311e3f52d7d2a61484524f91e3
KCIV <KCIV4@att.net>
2015-06-11 11:25:03 -0400

    Create Results.R
    Formula to display results from observed column in sexy way.

---
+++ R/Results.R
@@ -0,0 +1,21 @@
+Results <- 
+function(KSTable)
+{
+    percent <- function(x, digits = 2, format = "f", ...) {
+        paste0(formatC(100 * x, format = format, digits = digits, ...), "%")
+    }
+    
+    
+    value1 <- KSTable[1:1,3:3]
+    cat(KSTable[1:1,2:2], " Authors made ", percent(value1))
+    cat("\n")
+    value1 <- KSTable[2:2,3:3]
+    cat(KSTable[2:2,2:2], " Authors made ", percent(value1))
+    cat("\n")
+    value1 <- KSTable[3:3,3:3]
+    cat(KSTable[3:3,2:2], " Authors made ", percent(value1))
+    
+    
+    
+    
+}
