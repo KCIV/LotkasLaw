5a5fdf573188210b4e15c3f4539fcc528d866f4a
KCIV <KCIV4@att.net>
2015-06-19 14:53:30 -0400

    Update LotkasN.R
    removed the testing print() lines.

--- R/LotkasN.R
+++ R/LotkasN.R
@@ -1,21 +1,13 @@
 LotkasN <- function(Sums,FullTable)
 {
   N <- nrow(FullTable)
-  print(N)
   lx <- Sums[3]
-  print(lx)
   ly <- Sums[4]
-  print(ly)
   xy <- Sums[5]
-  print(xy)
   x2 <- Sums[6]
-  print(x2)
   lx2 <- lx^2
   top <- (N*xy) - (lx*ly)
-  print((N*xy))
-  print(top)
   bottom <- (N*x2) - (lx2)
-  print(bottom)
   Nfinal <- top/bottom
   return(Nfinal)
 }
